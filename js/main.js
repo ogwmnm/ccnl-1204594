866e99742c5bc858a4b4098b87b8d7946bdf836f
Minami Ogawa <ogwmnm@gmail.com>
2016-11-12 13:38:55 +0900

    Add back to top button

--- js/main.js
+++ js/main.js
@@ -2,4 +2,22 @@
 
   "use strict";
 
+  var $window = $(window),
+      $backToTopBtn = $("#js-back-to-top-btn");
+
+  // スクロールでTOPに戻るボタンを表示・非表示する
+  $window.on("scroll", function() {
+    if ($window.scrollTop() > 600) {
+      $backToTopBtn.fadeIn(500);
+    } else {
+      $backToTopBtn.fadeOut(500);
+    }
+  });
+
+  // TOPに戻るボタンを押したらアニメーションしてTOPに戻る
+  $backToTopBtn.on("click", function(e) {
+    e.preventDefault();
+    $("body, html").animate({ scrollTop: 0 }, 500);
+  });
+
 })();
